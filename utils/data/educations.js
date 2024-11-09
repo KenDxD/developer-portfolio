dba2e47afd331cf6bda2cbc5eb7d39c303aea3dc
KenDxD <41190270+KenDxD@users.noreply.github.com>
2024-11-10 00:59:22 +0800

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor of Science in Information Technology",
+    duration: "2021 - Present",
+    institution: "STI College General Santos",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
+    title: "STEM - Senior High School",
     duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    institution: "GSC SPED Integrated School",
   },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
 ]
