a412c917275f8bdf83ec6914794d4956184db9af
KenDxD <41190270+KenDxD@users.noreply.github.com>
2024-11-10 01:06:56 +0800

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -2,23 +2,15 @@ export const skillsData = [
   'HTML',
   'CSS',
   'Javascript',
-  'Typescript',
-  'React',
-  'Next JS',
-  'Tailwind',
-  'MongoDB',
-  'MySQL',
-  'PostgreSQL',
+  'PHP',
+  'C++',
+  'C#',
+  'JAVA',
+  'Javascript',
   'Git',
-  'AWS',
   'Bootstrap',
-  'Docker',
-  'Go',
-  'Figma',
-  'Firebase',
-  'MaterialUI',
-  'Nginx',
-  'Strapi'
+  'Apache',
+  'Python'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
