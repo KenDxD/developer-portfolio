965202a18a24a856c85be9ab0b2d077b8df3c668
KenDxD <41190270+KenDxD@users.noreply.github.com>
2024-11-10 00:57:49 +0800

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'kelvinjayjandog@icloud.com.com',
+    phone: '+639276148948',
+    address: 'General Santos City, Philippines 9500',
+    github: 'https://github.com/kendxd',
+    facebook: 'https://www.facebook.com/crimson.kendxd',
+    linkedIn: 'https://www.linkedin.com/in/kelvin-jay-jandog',
+    twitter: 'https://twitter.com/crimsonkendxd',
+    stackOverflow: '',
+    devUsername: ""
 }
